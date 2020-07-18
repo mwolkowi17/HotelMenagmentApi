5ec93ba66424ac61414c3f220c56f129c970d515
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-07-18 22:52:23 +0200

    users_view_going_on

--- HotelMenagmentClientNew/Controllers/UserController.cs
+++ HotelMenagmentClientNew/Controllers/UserController.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelMenagmentClientNew.Models;
+using HotelMenagmentClientNew.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelMenagmentClientNew.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
         {
-            return View();
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ICollection<GuestDTO> guestsToDisplay = await _userService.GetAll();
+
+            var model = new HotelViewModel()
+            {
+                GuestList = guestsToDisplay
+            };
+            /*var guests = from n in _userService.GetAll()
+                              select n;
+            var guestVM = new HotelViewModel
+            {
+                GuestList = singleguest.ToList()
+            };
+            return View(guestVM);*/
+            return View(model);
         }
     }
 }
---
+++ HotelMenagmentClientNew/Models/HotelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelMenagmentClientNew.Models
+{
+    public class HotelViewModel
+    {
+        public List<RoomDTO> RoomList { get; set; }
+        public ICollection<GuestDTO> GuestList { get; set; }
+        public List<ReservationDTO> ReservationList { get; set; }
+        public List<ReservationHistoryDTO> ReservationHistoryList { get; set; }
+        public List<ReservationDTO> ReservedForToday { get; set; }
+    }
+}

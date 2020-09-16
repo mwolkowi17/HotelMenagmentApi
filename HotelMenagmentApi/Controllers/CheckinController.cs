e6e64404f9dac9b4e845e70c364457b0337d2254
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-09-16 20:49:01 +0200

    checkin controller added

---
+++ HotelMenagmentApi/Controllers/CheckinController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelMenagmentApi.Data;
+using HotelMenagmentApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelMenagmentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckinController : ControllerBase
+    {
+        private readonly HotelMenagmentContext _context;
+
+        public CheckinController(HotelMenagmentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reservation>>> GetCheckins()
+        {
+            var roomreservedfortoday =  _context.Reserevations
+                                        .Where(n => n.Check_in.Date == DateTime.Today.Date)
+                                        .Include(c => c.Room)
+                                        .Include(c => c.Guest)
+                                        .ToListAsync();
+
+
+           
+
+           
+            return await roomreservedfortoday;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Room>>> GetRoomsOccupied()
+        {
+            var roomnotoccupiedtoday = _context.Rooms
+                                      .Where(m => m.Is_ocuppied == false)
+                                      .Include(c => c.Guest)
+                                      .ToListAsync();
+
+            return await roomnotoccupiedtoday;
+        }
+    }
+}

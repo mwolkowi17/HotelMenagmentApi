3b6b2906300178c0e33d9a324e4905c2c427c191
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-09-27 10:15:06 +0200

    small_issue_repaired

--- HotelMenagmentApi/Controllers/CheckoutController.cs
+++ HotelMenagmentApi/Controllers/CheckoutController.cs
@@ -32,6 +32,53 @@ namespace HotelMenagmentApi.Controllers
             return await roomsToDisplay;
         }
 
+        [HttpGet("checkout")]
+        public async Task<ActionResult<IEnumerable<Room>>> Checkout(int id)
+        {
+            var room = _context.Rooms
+                                  .Where(n => n.Is_ocuppied == true)
+                                  .Include(c => c.Guest)
+                                  .ToList();
+
+
+          
+
+
+            var roomtocheckout = _context.Rooms
+                                 .Where(n => n.RoomID == id)
+                                 .Include(c => c.Guest)
+                                 .ToList();
+            //logowanie błędów
+            try
+            {
+                roomtocheckout.FirstOrDefault().Is_ocuppied = false;
+                roomtocheckout.FirstOrDefault().Guest = null;
+                await _context.SaveChangesAsync();
+            }
+            catch 
+            {
+               
+
+                throw;
+            }
+
+            //new segment - to tests - to refine
+            var reservationToCheckout = _context.Reserevations
+                                        .Where(n => n.Room == roomtocheckout.FirstOrDefault())
+                                        .FirstOrDefault();
+
+            var numberOfDaysInHotel = DateTime.Now.DayOfYear - reservationToCheckout.Check_in.DayOfYear;
+
+            var chargeForStayInHotel = numberOfDaysInHotel * roomtocheckout.FirstOrDefault().ReguralPrice;
+            if (reservationToCheckout.BreakfestIncluded == true)
+            {
+                chargeForStayInHotel = (numberOfDaysInHotel * roomtocheckout.FirstOrDefault().ReguralPrice) + (numberOfDaysInHotel * 80);
+            }
+            // end of ne segment
+           
+            //return RedirectToAction(nameof(Index));
+            return  roomtocheckout;
+        }
 
     }
 }
